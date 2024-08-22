473b5101beda12541b773d3f2c3e0c2a10a90e76
GabrieleRuggieri <gabri20022002@gmail.com>
2024-08-22 17:20:53 +0200

    Model

---
+++ ChatRPS/src/main/java/com/chatbot/model/User.java
@@ -0,0 +1,34 @@
+package com.chatbot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
