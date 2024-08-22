ead5fbc8093a69824270ca417e2f723887d658e0
nameisalfio <alfio.spoto@outlook.it>
2024-08-22 15:57:22 +0200

    First commit

---
+++ ChatRPS/src/main/java/com/chatbot/ChatRpsApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRpsApplication.class, args);
+	}
+
+}
